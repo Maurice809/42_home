dd874f53e38afafec07bde308f907b0e076ce53f
Maurice809 <maurice809@hotmail.com>
2022-10-31 11:25:39 +0100

    GNL exame update

---
+++ get_next_line/exam-2022/compile.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+clear
+make re
+./get_next_line
+rm get_next_line
--- get_next_line/exam-2022/get_next_line.h
+++ get_next_line/exam-2022/get_next_line.h
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: tmoret <marvin@42lausanne.ch>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/10/22 18:30:03 by tmoret            #+#    #+#             */
-/*   Updated: 2022/10/27 23:19:49 by tmoret           ###   ########.fr       */
+/*   Created: 2022/10/31 10:01:30 by tmoret            #+#    #+#             */
+/*   Updated: 2022/10/31 10:19:36 by tmoret           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,22 +18,22 @@
 # include <stdlib.h>
 
 # if !defined(BUFFER_SIZE) || BUFFER_SIZE <= 0
-#  error "BUFFER_SIZE is either, not defined, equals zero or negative."
+#  error "BUFFER_SIZE is either, not defined, equals zero or negative"
 #  undef BUFFER_SIZE
 #  define BUFFER_SIZE 0
 # endif
 
 typedef struct s_read_info
 {
-	int		fd;
-	int		pos;
-	int		max;
-	char	data[BUFFER_SIZE];
-}	t_read_info;
+	int fd;
+	int pos;
+	int max;
+	char data[BUFFER_SIZE];
+} t_read_info;
 
-char	*get_next_line(int fd);
-int		ft_init(int fd, t_read_info *info);
-int		ft_single(t_read_info *info, char *res);
-char	*ft_strjoin(char *s, char c);
+char *get_next_line(int fd);
+int ft_init(int fd, t_read_info *buff);
+int ft_read(t_read_info *info, char *res);
+char *ft_strjoin(char *s, char c);
 
 #endif
